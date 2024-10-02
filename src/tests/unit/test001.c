1227073f7850bc753e2e6949aa5298e161b343c9
Mike Smullin <mike@mikesmullin.com>
2024-10-02 10:29:02 -0600

    learning about EPSILON for floats

--- src/tests/unit/test001.c
+++ src/tests/unit/test001.c
@@ -1,14 +1,36 @@
 #include "test001.h"
 
+#include <float.h>
+#include <math.h>
+#include <stdio.h>
+
 #include "../../lib/Base.h"
 #include "../../lib/Breakpoint.h"
 
+static f32 safe_division(f32 numerator, f32 denominator) {
+  // Check if y is close to zero, then clamp the value of z
+  if (fabs(denominator) < FLT_EPSILON) {
+    return 0.0f;  // Clamp z when y is too small
+  }
+  return numerator / denominator;
+}
+
 static void Test001__Test() {
   LOG_DEBUGF("Test001");
-  LOG_DEBUGF("a");
-  DEBUGGER();
-  LOG_DEBUGF("b");
-  LOG_DEBUGF("c");
+
+  f32 steps = 10.0f;
+  f32 b = 1.0f;
+  f32 a = -b;
+  f32 step = (b - a) / steps;
+  for (f32 y = a; y <= b; y += step) {
+    for (f32 x = a; x <= b; x += step) {
+      f32 z = safe_division(x, y);
+      if (y < 0) z = -z;
+
+      printf("% 5.3f ", z);
+    }
+    printf("\n");
+  }
 }
 
 void Test__Boot() {
