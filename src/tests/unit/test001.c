1f85c08cabed2d98c06805dfbe6b96f78b1f104b
Mike Smullin <mike@mikesmullin.com>
2024-10-07 00:27:35 -0600

    [stategraph] initlal import

--- src/tests/unit/test001.c
+++ src/tests/unit/test001.c
@@ -1,210 +1,192 @@
 #include "test001.h"
 
-#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "../../lib/Base.h"
 #include "../../lib/Breakpoint.h"
-#include "../../lib/Log.h"
-//#include "../../lib/BehaviorTree.h"
 
-// Define return statuses for behavior tree nodes
-typedef enum { BT_SUCCESS, BT_FAILURE, BT_RUNNING } BTStatus;
+// Define Event types
+typedef enum { EVENT_NONE, EVENT_DEATH, EVENT_ATTACKED, EVENT_ANIMOVER } EventType;
 
-// Basic structure for a behavior tree node
-typedef struct BTNode {
-  // TODO: maybe easier to return void, and optionally edit node->state
-  // BTStatus state;
-  // TODO: use Dispatcher() fn pattern, rather than fn* (for hot-reload)
-  BTStatus (*tick)(struct BTNode* node);  // Function pointer to the behavior's tick function
-} BTNode;
+typedef struct State State;
+typedef struct StateGraph StateGraph;
 
-// Selector Node: Runs child behaviors in sequence until one succeeds
-typedef struct {
-  BTNode base;
-  BTNode** children;
-  int childCount;
-} SelectorNode;
+typedef void (*StateFunction)(StateGraph* sg);
 
-BTStatus SelectorTick(SelectorNode* node) {
-  for (int i = 0; i < node->childCount; ++i) {
-    BTNode* child = node->children[i];
-    BTStatus status = child->tick(child);
-    if (status == BT_SUCCESS || status == BT_RUNNING) {
-      return status;  // Return success or running if any child succeeds
-    }
+// Define the structure for State
+struct State {
+  const char* name;
+  // TODO: tags set in each state are what allow states to intelligently transit
+  // const char* tags[];
+  // bool State__HasTag(const char* tag);
+  StateFunction onEnter;
+  StateFunction onUpdate;
+  StateFunction onExit;
+  // TODO: event listeners can also transit state
+  // Event events[];
+  // TODO: anim timers/onFrameN can also transit state (like Flash keyframes)
+  // Timer timeline[];
+  struct State* nextState;  // Next state to transition to
+};
+
+// Define the structure for StateGraph
+struct StateGraph {
+  State* currentState;
+  EventType currentEvent;  // Event sent to the StateGraph
+};
+
+// Function prototypes for state functions
+void idleoutEnter(StateGraph* sg);
+void idleoutUpdate(StateGraph* sg);
+void idleoutExit(StateGraph* sg);
+void idleinEnter(StateGraph* sg);
+void idleinUpdate(StateGraph* sg);
+void idleinExit(StateGraph* sg);
+void emergeEnter(StateGraph* sg);
+void emergeUpdate(StateGraph* sg);
+void emergeExit(StateGraph* sg);
+void hibernateEnter(StateGraph* sg);
+void hibernateUpdate(StateGraph* sg);
+void hibernateExit(StateGraph* sg);
+
+// Define states
+State idleoutState, idleinState, emergeState, hibernateState;
+
+// State definitions with transitions
+State idleoutState = {"IdleOut", idleoutEnter, idleoutUpdate, idleoutExit, &hibernateState};
+State idleinState = {"IdleIn", idleinEnter, idleinUpdate, idleinExit, &emergeState};
+State emergeState = {"Emerge", emergeEnter, emergeUpdate, emergeExit, &idleinState};
+State hibernateState = {"Hibernate", hibernateEnter, hibernateUpdate, hibernateExit, &idleoutState};
+
+// StateGraph initialization
+// TODO: accept params like (name, states, events, initialState)
+StateGraph* createStateGraph() {
+  StateGraph* sg = (StateGraph*)malloc(sizeof(StateGraph));
+  sg->currentState = &idleoutState;  // Start in the idleout state
+  sg->currentEvent = EVENT_NONE;  // No event initially
+  return sg;
+}
+
+// Event handling functions
+void sendEvent(StateGraph* sg, EventType event) {
+  sg->currentEvent = event;
+}
+
+// Check for specific events and handle state transitions
+void handleEvents(StateGraph* sg) {
+  switch (sg->currentEvent) {
+    case EVENT_DEATH:
+      printf("Handled 'death' event.\n");
+      // Transition to hibernate immediately on death
+      sg->currentState->onExit(sg);
+      sg->currentState = &hibernateState;
+      sg->currentState->onEnter(sg);
+      break;
+    case EVENT_ATTACKED:
+      printf("Handled 'attacked' event.\n");
+      // Transition to emerge when attacked
+      sg->currentState->onExit(sg);
+      sg->currentState = &emergeState;
+      sg->currentState->onEnter(sg);
+      break;
+    case EVENT_ANIMOVER:
+      printf("Handled 'animover' event.\n");
+      // Use animover to move to the next state
+      sg->currentState->onExit(sg);
+      sg->currentState = sg->currentState->nextState;
+      sg->currentState->onEnter(sg);
+      break;
+    default:
+      break;
   }
-  return BT_FAILURE;  // Fail if all children fail
+  sg->currentEvent = EVENT_NONE;  // Reset the event after handling
 }
 
-// Function to create a selector node
-SelectorNode* CreateSelectorNode(BTNode** children, int count) {
-  SelectorNode* node = (SelectorNode*)malloc(sizeof(SelectorNode));
-  node->children = children;
-  node->childCount = count;
-  node->base.tick = (BTStatus(*)(BTNode*))SelectorTick;
-  return node;
+// State functions implementation
+void idleoutEnter(StateGraph* sg) {
+  printf("Entering IdleOut State.\n");
 }
 
-// -- PigEntity --
+void idleoutUpdate(StateGraph* sg) {
+  printf("Update IdleOut State.\n");
+  // For this example, after one update, we'll simulate animation over
 
-// Pig's brain states
-// TODO: Only use BTStatus, otherwise use component state
-typedef enum { PIG_IDLE, PIG_PANIC, PIG_FOLLOW_LEADER } PigState;
-
-typedef struct {
-  PigState state;  // Current state of the pig
-  bool isThreatNear;  // Flag to indicate if a threat is nearby
-  bool isLeaderNear;  // Flag to indicate if a leader is nearby
-} Pig;
-
-// #metacode
-// #macro MACRO1(T1)
-//   {{~#for T1~}}
-//   // --- {{this.nn}}Node ---
-//   typedef struct {
-//     BTNode base;
-//     {{this.e}};
-//   } {{this.nn}}Node;
-//   BTStatus {{this.nn}}Tick({{this.nn}}Node* node) {
-//     {{this.e}} = node->{{this.v}};
-//     printf("{{this.msg}}\n");
-//     if ({{this.v}}->{{this.c1}}) {
-//       return {{this.o1}};  // {{this.ex1}}
-//     }
-//     return {{this.o2}}; // {{this.ex2}}
-//   }
-//   {{this.nn}}Node* Create{{this.nn}}Node({{this.e}}) {
-//     {{this.nn}}Node* node = malloc(sizeof({{this.nn}}Node));
-//     node->{{this.v}} = {{this.v}};
-//     node->base.tick = (BTStatus(*)(BTNode*)){{this.nn}}Tick;
-//     return node;
-//   }
-//   {{~/for~}}
-//
-// #table T_VARS
-//   nn              | e        | v   | msg                            | c1           | o1         | ex1                                                      | o2         | ex2                                           |
-//   PigIdle         | Pig* pig | pig | Pig is idling...               | isThreatNear | BT_FAILURE | Fail and switch to another state if a threat is detected | BT_SUCCESS |                                               |
-//   PigPanic        | Pig* pig | pig | Pig is panicking!              | isThreatNear | BT_RUNNING | Keep running in panic mode if the threat is still nearby | BT_SUCCESS | Calm down if the threat is gone               |
-//   PigFollowLeader | Pig* pig | pig | Pig is following the leader... | isLeaderNear | BT_RUNNING | Continue following the leader                            | BT_FAILURE | If leader is gone, fail and pick a new action |
-//
-// MACRO1(T_VARS)
-// #metagen
-
-// --- PigIdleNode ---
-typedef struct {
-  BTNode base;
-  Pig* pig;
-} PigIdleNode;
-
-BTStatus PigIdleTick(PigIdleNode* node) {
-  Pig* pig = node->pig;
-  // printf("Pig is considering idling...\n");
-  // if (!pig->isThreatNear) {
-  printf("Pig is idling...\n");
-  pig->state = PIG_IDLE;
-  return BT_RUNNING;  // Fail and switch to another state if a threat is detected
-  // }
-  // return BT_SUCCESS;  // We're the last node, so don't try to do anything else
+  // TODO: the method which triggers play for all animation,
+  //       is what sends out the event (when its actually done)
+  // inst.AnimState:PlayAnimation("pick")
+  sendEvent(sg, EVENT_ANIMOVER);
+  // TODO: but states can trigger other (incl. custom events) (ie. "freshspawn")
 }
 
-// TODO: maybe easier to not define all the Create*() fns, and just manually init structs
-PigIdleNode* CreatePigIdleNode(Pig* pig) {
-  PigIdleNode* node = malloc(sizeof(PigIdleNode));
-  node->pig = pig;
-  node->base.tick = (BTStatus(*)(BTNode*))PigIdleTick;
-  return node;
+void idleoutExit(StateGraph* sg) {
+  printf("Exiting IdleOut State.\n");
 }
 
-// --- PigPanicNode ---
-typedef struct {
-  BTNode base;
-  Pig* pig;
-} PigPanicNode;
+void idleinEnter(StateGraph* sg) {
+  printf("Entering IdleIn State.\n");
+}
 
-BTStatus PigPanicTick(PigPanicNode* node) {
-  Pig* pig = node->pig;
-  printf("Pig is thinking about panicking...\n");
-  if (pig->isThreatNear) {
-    printf("Pig is panicking!\n");
-    pig->state = PIG_PANIC;
-    return BT_RUNNING;  // Keep running in panic mode if the threat is still nearby
+void idleinUpdate(StateGraph* sg) {
+  printf("Update IdleIn State.\n");
+  if (rand() % 10 < 2) {  // 20% chance to be attacked
+    sendEvent(sg, EVENT_ATTACKED);
+  } else {
+    sendEvent(sg, EVENT_ANIMOVER);
   }
-  return BT_FAILURE;  // Do other things if the threat is gone
 }
 
-PigPanicNode* CreatePigPanicNode(Pig* pig) {
-  PigPanicNode* node = malloc(sizeof(PigPanicNode));
-  node->pig = pig;
-  node->base.tick = (BTStatus(*)(BTNode*))PigPanicTick;
-  return node;
+void idleinExit(StateGraph* sg) {
+  printf("Exiting IdleIn State.\n");
 }
 
-// --- PigFollowLeaderNode ---
-typedef struct {
-  BTNode base;
-  Pig* pig;
-} PigFollowLeaderNode;
+void emergeEnter(StateGraph* sg) {
+  printf("Entering Emerge State.\n");
+}
 
-BTStatus PigFollowLeaderTick(PigFollowLeaderNode* node) {
-  Pig* pig = node->pig;
-  printf("Pig is considering following a leader...\n");
-  if (pig->isLeaderNear) {
-    printf("Pig is following the leader!\n");
-    pig->state = PIG_FOLLOW_LEADER;
-    return BT_RUNNING;  // Continue following the leader
+void emergeUpdate(StateGraph* sg) {
+  printf("Update Emerge State.\n");
+  sendEvent(sg, EVENT_ANIMOVER);  // After emerge, transition to idlein
+}
+
+void emergeExit(StateGraph* sg) {
+  printf("Exiting Emerge State.\n");
+}
+
+void hibernateEnter(StateGraph* sg) {
+  printf("Entering Hibernate State.\n");
+}
+
+void hibernateUpdate(StateGraph* sg) {
+  printf("Update Hibernate State.\n");
+  if (rand() % 10 < 3) {  // 30% chance to come out of hibernation
+    sendEvent(sg, EVENT_ANIMOVER);
   }
-  return BT_FAILURE;  // Pick a new action, while leader is gone
 }
 
-PigFollowLeaderNode* CreatePigFollowLeaderNode(Pig* pig) {
-  PigFollowLeaderNode* node = malloc(sizeof(PigFollowLeaderNode));
-  node->pig = pig;
-  node->base.tick = (BTStatus(*)(BTNode*))PigFollowLeaderTick;
-  return node;
+void hibernateExit(StateGraph* sg) {
+  printf("Exiting Hibernate State.\n");
 }
-// #metaend
 
-// Simulate the Behavior Tree running for the pig
-void RunPigBrain(Pig* pig) {
-  // Create leaf nodes for each behavior
-  PigIdleNode* idle = CreatePigIdleNode(pig);
-  PigPanicNode* panic = CreatePigPanicNode(pig);
-  PigFollowLeaderNode* followLeader = CreatePigFollowLeaderNode(pig);
-
-  // Create the selector node with behaviors
-  BTNode* behaviors[] = {(BTNode*)panic, (BTNode*)followLeader, (BTNode*)idle};
-  SelectorNode* root = CreateSelectorNode(behaviors, 3);
-
-  // Run the behavior tree in a loop (for demonstration)
-  for (int i = 0; i < 10; i++) {
-    printf("---- TICK %d ----\n", i);
-    BTStatus result = root->base.tick((BTNode*)root);
-
-    if (result == BT_SUCCESS) {
-      printf("Behavior completed successfully!\n");
-    } else if (result == BT_RUNNING) {
-      printf("Behavior is running...\n");
-    } else {
-      printf("Behavior failed!\n");
-    }
-
-    // Simulate changing world conditions
-    pig->isThreatNear = (i == 3 || i == 4);  // Simulate a threat near at ticks 3 and 4
-    pig->isLeaderNear = (i > 6);  // Simulate leader being near after tick 6
+// Main simulation loop
+void runStateGraph(StateGraph* sg) {
+  for (int i = 0; i < 20; i++) {
+    printf("--TICK %d--\n", i);
+    sg->currentState->onUpdate(sg);
+    handleEvents(sg);  // Handle events and trigger transitions
   }
-
-  // Cleanup
-  free(idle);
-  free(panic);
-  free(followLeader);
-  free(root);
 }
 
 static void Test001__Test() {
-  LOG_DEBUGF("Test001 BehaviorTree System Core");
-  Pig pig = {PIG_IDLE, false, false};  // Initialize pig with default state (idle)
-  RunPigBrain(&pig);  // Run the Pig's brain using the behavior tree
+  LOG_DEBUGF("Test001 StateGraph System Demo");
+  StateGraph* sg = createStateGraph();
+  sg->currentState->onEnter(sg);  // Enter the initial state
+
+  // Run the state graph simulation (in reality, this could be part of a game loop)
+  runStateGraph(sg);
+
+  // Free allocated memory
+  free(sg);
 }
 
 void Test__Boot() {
